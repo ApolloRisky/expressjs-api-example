20a6029adbe663ea108b66c578167cc302b89135
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2020-12-18 00:47:05 +0700

    feat: build api endpoint for update a post, delete a post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,26 +3,17 @@ const APIError = require('../utils/APIError');
 const Post = require('../models/post.model');
 const { postSerializer, postCollectionSerializer } = require('../serializers/post.serializer');
 
+// Get list posts
 exports.listPost = async (req, res, next) => {
   try {
     const posts = await Post.find();
-    res.json(postCollectionSerializer(posts));
+    res.json({ list: postCollectionSerializer(posts) });
   } catch (error) {
     next(error);
   }
 };
 
-exports.createPost = async (req, res, next) => {
-  try {
-    const post = new Post(req.body);
-    const savedPost = await post.save();
-
-    res.json(postSerializer(savedPost));
-  } catch (error) {
-    next(error);
-  }
-};
-
+// Get post by ID
 exports.showPost = async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -33,7 +24,53 @@ exports.showPost = async (req, res, next) => {
         message: 'Post not found',
       });
     }
-    res.json(postSerializer(post));
+    res.json({ item: postSerializer(post) });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Create post
+exports.createPost = async (req, res, next) => {
+  try {
+    const post = new Post(req.body);
+    const savedPost = await post.save();
+
+    res.json({ item: postSerializer(savedPost) });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update post by ID
+exports.updatePost = async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!post) {
+      throw new APIError({
+        status: httpStatus.NOT_FOUND,
+        message: 'Post not found',
+      });
+    }
+    res.json({ item: postSerializer(post) });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete post by ID
+exports.removePost = async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndRemove(req.params.id);
+    if (!post) {
+      throw new APIError({
+        status: httpStatus.NOT_FOUND,
+        message: 'Post not found',
+      });
+    }
+    res.json({
+      deleted: postSerializer(post),
+    });
   } catch (error) {
     next(error);
   }
